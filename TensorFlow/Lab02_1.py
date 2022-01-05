ba872ae40e9609f08238fba4d9771fd575b1dde6
Ah-yeong <84187163+Ah-yeong@users.noreply.github.com>
2022-01-05 12:04:11 +0900

    Create Lab02_1.py

---
+++ TensorFlow/Lab02_1.py
@@ -0,0 +1,20 @@
+import tensorflow as tf
+import numpy as np
+
+x_data = [1,2,3,4,5]
+y_data = [2,4,6,8,10]
+
+W = tf.Variable(2.9)
+b = tf.Variable(0.5)
+
+learning_rate = 0.01
+
+for i in range(200+1):
+    with tf.GradientTape() as tape:
+        hypothesis = W * x_data + b
+        cost = tf.reduce_mean(tf.square(hypothesis - y_data))
+    W_grad, b_grad = tape.gradient(cost, [W,b])
+    W.assign_sub(learning_rate * W_grad)
+    b.assign_sub(learning_rate * b_grad)
+    if i % 10 == 0:
+        print("{:5}|{:10.4f}|{:10.4}|{:10.6f}".format(i,W.numpy(), b.numpy(), cost))
